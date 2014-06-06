5e45ec56c07eb058935edced18cfda483a2a121b
Maxim Smolin <maximsmol@gmail.com>
2014-06-06 16:34:08 +0400

    Misc

--- jsonex.js
+++ jsonex.js
@@ -1,412 +1,561 @@
 'use strict';
 
-// Globals
-var string, handlers, curCharI, lineCount, context;
-
-//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-
-//Special
-var parserBroken, getCurChar, trySkipComment, nextChar, expected, expectedChar, expect, unexpected;
-
-//Checkers
-var isCurCharFinal, isCharSpace, isCurCharSpace, isCharQuote, isCurCharQuote, isCharSpecial, isCurCharSpecial;
-
-//Readers
-var readNum, parseUnicode, readString, readStringValue, readListTo, readKey, readArray, readObject, parseHandler, readValue;
-
-//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-
-//Special
-
-//This version of a parser is propably broken...
-parserBroken = function (msg)
+var JsonexParser = function(handlers, initialContext)
 {
-	throw new Error('Jsonex parser is broken: ' + msg + '!');
+	if (!handlers) handlers = {};
+	if (!initialContext) initialContext = {};
+
+	this.handlers = handlers;
+	this.initialContext = initialContext;	
 };
 
-//Returns the current character, checking if it is valid
-getCurChar = function ()
+JsonexParser.prototype.parse = function(string)
 {
-	if (!string[curCharI])
-		parserBroken ('current character is invalid (' + string[curCharI] + ')');
-	return string[curCharI];
-};
+	try{
+		if (!string) return;
 
-//Tries to skip a comment
-trySkipComment = function ()
-{
-	if (getCurChar() != '/') return;
-	nextChar(false, true);
+		this.string = string;
+		this.charIndex = 0;
+		this.lineNumber = 0;
 
-	if (getCurChar() == '*')
-	{
-		nextChar(false, true);
-
-		readString('*');
-		nextChar(false, true);
-
-		expect('/');
-		nextChar(false, true);
+		return this.parseValue(this.initialContext);
 	}
-	else if (getCurChar() == '/')
+	catch(e)
 	{
-		readString('\n');
-		nextChar(false, true);
+		console.log('Error at:', this.lineNumber + ':' + this.charIndex % this.lineNumber);
+		throw e;
 	}
 };
 
-//Moves current character pointer onde char, checking if it is in bounds of a string
-nextChar = function (doNotSkipSpace, skippingComment)
-{
-	if (getCurChar() == '\n') lineCount++;
-	curCharI++;
-	if (string.length <= curCharI || curCharI < 0)
-		parserBroken ('current character index out of string bounds (' + lineCount + ':' + curCharI%lineCount + ')');	
+module.exports = JsonexParser;
 
-	if (skippingComment !== true) trySkipComment();
-	if (doNotSkipSpace !== true && isCurCharSpace()) nextChar();
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+JsonexParser.prototype.parseDictionary = function(context)
+{
+	this.expect('{');
+	this.next();
+	this.skipSpaces();
+
+	var res = {};
+	while (true)
+	{
+		var key = this.parseKey();
+		this.skipSpaces();
+
+		this.expect(':');
+		this.next();
+		this.skipSpaces();
+
+		res[key] = this.parseValue(context);
+		this.skipSpaces();
+
+		if (this.charIs(','))
+		{
+			this.next();
+			this.skipSpaces();
+		}
+		if (this.charIs('}')) break;
+		if (this.charIndex >= this.string.length) this.expect('}');
+	}
+
+	this.expect('}');
+	this.next();
+	this.skipSpaces();
+
+	return res;
 };
 
-//Throws an error, if current character is not as expected
-expect = function (char)
+JsonexParser.prototype.parseArray = function(context)
 {
-	if (getCurChar() != char) expectedChar(char);
+	this.expect('[');
+	this.next();
+	this.skipSpaces();
+
+	var res = [];
+	this.readList(res, ']', context);
+
+	this.expect(']');
+	this.next();
+	this.skipSpaces();
+
+	return res;
 };
 
-//Call to send a message, when current character is not as expected
-expectedChar = function (char)
+JsonexParser.prototype.parseKey = function()
 {
-	expected('\'' + char + '\'');
+	if (this.charIs('"')) return this.readQuoteString();
+	else if (this.charIs('\'')) return this.readApostropheString();
+	else return this.readJsonexKey();
 };
 
-//Call to send a message, when something is not as expected
-expected = function (msg)
+JsonexParser.prototype.parseCallable = function(startWith, context)
 {
-	throw new Error('Expected ' + msg + ' at ' + lineCount + ':' + curCharI%lineCount + ', but found: ' + getCurChar() + '!');
+	if (this.charIsInvalidForName()) this.expect();
+
+	var callableName;
+	if (!startWith) callableName = ''; 
+	else callableName = startWith;
+
+	while (true)
+	{
+		if (this.char() == '(') break;
+		if (this.charIsSpace())
+		{
+			this.skipSpaces();
+			this.expect('(');
+			break;
+		}
+		if (this.charIsInvalidForName()) this.expect();
+
+		callableName += this.char();
+		this.next();
+
+		if (this.charIndex >= this.string.length) this.expect('(');
+	}
+	this.expect('(');
+	this.next();
+	this.skipSpaces();
+
+	var args = [];
+	this.readList(args, ')');
+
+	this.expect(')');
+	this.next();
+	this.skipSpaces();
+
+	var res;
+	if (Object.keys(this.handlers).indexOf(callableName) == -1)
+	{
+		if (Object.keys(this.handlers).indexOf('*') == -1) throw new SyntaxError('Undefined handler ' + callableName);
+		args.unshift(callableName);
+		args.unshift(context);
+		res = this.handlers['*'].apply(undefined, args);
+	}
+	else
+	{
+		args.unshift(context);
+		res = this.handlers[callableName].apply(undefined, args);
+	}
+
+	return res;
 };
 
-//Call to tell user, when current character is not as expected
-unexpected = function ()
+JsonexParser.prototype.parseEscaped = function()
 {
-	throw new Error('Unexpected character at ' + lineCount + ':' + curCharI%lineCount + ' \'' + getCurChar() + '\'!');
+	var res = '';
+	if (this.charIs('u'))
+	{
+		this.next();
+		for (var i = 0; i < 4; i++)
+		{
+			if (isNaN(parseInt(this.char(), 16))) this.illegal();
+
+			res += this.char();
+			this.next();
+		}
+
+		res = String.fromCharCode(parseInt(res, 16));
+	}
+	else
+	{
+		if (this.charIs('b'))
+		{
+			res = '\b';
+			this.next();
+		}
+		else if (this.charIs('f'))
+		{
+			res = '\f';
+			this.next();
+		}
+		else if (this.charIs('n'))
+		{
+			res = '\n';
+			this.next();
+		}
+		else if (this.charIs('r'))
+		{
+			res = '\r';
+			this.next();
+		}
+		else if (this.charIs('t'))
+		{
+			res = '\t';
+			this.next();
+		}
+		else
+		{
+			res = this.char();
+			this.next();
+		}
+	}
+
+	
+	return res;
+};
+
+JsonexParser.prototype.parseReservedAndCallables = function(context)
+{
+	var res, resStr = '';
+	if (this.charIs('n'))
+	{
+		resStr += this.char();
+		this.next();
+
+		if (this.charIs('u'))
+		{
+			resStr += this.char();
+			this.next();
+
+			if (this.charIs('l'))
+			{
+				resStr += this.char();
+				this.next();
+
+				if (this.charIs('l'))
+				{
+					resStr += this.char();
+					this.next();
+
+					res = null;
+				}
+			}
+		}
+	}
+	else if (this.charIs('t'))
+	{
+		resStr += this.char();
+		this.next();
+
+		if (this.charIs('r'))
+		{
+			resStr += this.char();
+			this.next();
+
+			if (this.charIs('u'))
+			{
+				resStr += this.char();
+				this.next();
+
+				if (this.charIs('e'))
+				{
+					resStr += this.char();
+					this.next();
+
+					res = true;
+				}
+			}
+		}
+	}
+	else if (this.charIs('f'))
+	{
+		resStr += this.char();
+		this.next();
+
+		if (this.charIs('a'))
+		{
+			resStr += this.char();
+			this.next();
+
+			if (this.charIs('l'))
+			{
+				resStr += this.char();
+				this.next();
+
+				if (this.charIs('s'))
+				{
+					resStr += this.char();
+					this.next();
+
+					if (this.charIs('e'))
+					{
+						resStr += this.char();
+						this.next();
+
+						res = false;
+					}
+				}
+			}
+		}
+	}
+	else return this.parseCallable('', context);
+
+	if (!this.charIsInvalidForName()) return this.parseCallable(resStr, context);
+
+	this.skipSpaces();
+	return res;
+};
+
+JsonexParser.prototype.parseValue = function(context)
+{
+	if (this.charIs('{')) return this.parseDictionary(context);
+	else if (this.charIs('[')) return this.parseArray(context);
+	else if (this.charIs('"')) return this.readQuoteString();
+	else if (this.charIs('\'')) return this.readApostropheString();
+	else if (!isNaN(parseInt(this.char())) || this.charIs('-')) return this.readNum();
+	else return this.parseReservedAndCallables(context);
 };
 
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
-//Checkers
-
-//Returns true if current character is the last in the string
-isCurCharFinal = function ()
+JsonexParser.prototype.readStringContents = function(quoteChar)
 {
-	return curCharI == string.length - 1;
+	var res = '', escaped = false;
+	while (true)
+	{
+		if (!escaped && this.char() == quoteChar) break;
+		if (!escaped && this.char() == '\\') 
+		{
+			escaped = true;
+			this.next();
+		}
+
+		if (escaped)
+		{
+			res += this.parseEscaped();
+			escaped = false;
+		}
+		else
+		{
+			res += this.char();
+			this.next();
+		}
+		
+
+		if (this.charIndex >= this.string.length) this.expect(quoteChar);
+	}
+
+	return res;
 };
 
-//Returns true if the character is considered a 'space' character (new line / tab / space)
-isCharSpace = function (char)
+JsonexParser.prototype.readQuoteString = function()
 {
-	return char == '\t' || char == '\n' || char == ' ';
+	this.expect('"');
+	this.next();
+
+	var res = this.readStringContents('"');
+
+	this.expect('"');
+	this.next();
+	this.skipSpaces();
+
+	return res;
 };
 
-//Returns true if current character is considered a 'space' character (new line / tab / space)
-isCurCharSpace = function ()
+JsonexParser.prototype.readApostropheString = function()
 {
-	return isCharSpace(getCurChar());
+	this.expect('\'');
+	this.next();
+
+	var res = this.readStringContents('\'');
+
+	this.expect('\'');
+	this.next();
+	this.skipSpaces();
+
+	return res;
 };
 
-//Returns true if the character is a quote
-isCharQuote = function (char)
+JsonexParser.prototype.readNum = function()
 {
-	if (char == '\'') return '\'';
-	if (char == '"') return '"';
-	return null;
-};
+	var res = 0, fraction = 0, parsingFraction = false, negative = false, parsingNotation = false, mathNotation = 0;
 
-//Returns true if current character is a quote
-isCurCharQuote = function ()
-{
-	return isCharQuote(getCurChar());
-};
-
-//Returns true if the character is a special character for json
-isCharSpecial = function (char)
-{
-	return isCharQuote(char) || char == '{' || char == '}' || char == '[' || char == ']' || char == '(' || char == ')';
-};
-
-//Returns true if current character is a special character for json
-isCurCharSpecial = function ()
-{
-	return isCharSpecial(getCurChar());
-};
-
-//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-
-//Readers
-
-//Reads a number
-readNum = function ()
-{
-	var res = 0, fraction = 0, parsingFraction = false, negative = false;
-
-	if (getCurChar() == '-')
+	if (this.char() == '-')
 	{
 		negative = true;
-		nextChar();
+		this.next();
+		this.skipSpaces();
 	}
 
 	var curInt;
 	while (true)
 	{
-		if (getCurChar() == '.')
+		if (this.charIs('.'))
 		{
 			parsingFraction = true;
-			nextChar(true);
+			this.next();
 		}
-		else if (!isNaN((curInt = parseInt(getCurChar()))))
+		else if (this.charIs('e'))
 		{
-			if (parsingFraction)
-			{
-				fraction *= 10;
-				fraction += curInt;
-
-				nextChar(true);
-			}
-			else
-			{
-				res *= 10;
-				res += curInt;
-
-				nextChar(true);
-			}
+			parsingFraction = false;
+			parsingNotation = true;
+			this.next();
 		}
-		else break;
+
+		if (isNaN(( curInt = parseInt(this.char()) )) ) break;
+		if (parsingFraction)
+		{
+			fraction *= 10;
+			fraction += curInt;
+		}
+		else if (parsingNotation)
+		{
+			mathNotation *= 10;
+			mathNotation += curInt;
+		}
+		else
+		{
+			res *= 10;
+			res += curInt;
+		}
+		this.next();
 	}
 
 	var result = res + fraction / Math.pow(10, fraction.toString().length);
+	for (var i = 0; i < mathNotation; i++)
+	{
+		result *= 10;
+	}
+
 	return negative ? -result : result;
 };
 
-parseUnicode = function()
+JsonexParser.prototype.readList = function(readTo, endChar, context)
 {
-	expect('u');
-	nextChar();
+	while (true)
+	{
+		if (this.charIs(endChar)) break;
+
+		readTo.push(this.parseValue(context));
+		this.skipSpaces();
+
+		if (this.charIs(','))
+		{
+			this.next();
+			this.skipSpaces();
+
+			if (this.charIs(endChar)) break;
+			if (this.charIndex >= this.string.length) this.expect(endChar);
+		}
+	}
+};
+
+JsonexParser.prototype.readJsonexKey = function()
+{
+	if (this.charIsInvalidForName()) this.expect();
 
 	var res = '';
-
-	for (var i = 0; i < 4; i++)
-	{
-		var curChar = getCurChar();
-		if (isNaN(parseInt(curChar)) && curChar != 'A' & curChar != 'B' && curChar != 'C' && curChar != 'D' && curChar != 'E' && curChar != 'F' &&
-			curChar != 'a' && curChar != 'b' && curChar != 'c' && curChar != 'd' && curChar != 'e' && curChar != 'f') expected('a hexidecimal character');
-
-		res += curChar;
-		nextChar();
-	}
-
-	return String.fromCharCode(parseInt(res, 16));
-};
-
-//Reads a string ending with endChar and allowing special characters to be in quoted strings
-readString = function (endChar, quoted)
-{
-	var res = '', readingToEnd = false, escaping = false;
-
-	if (!endChar) readingToEnd = true;
-
 	while (true)
 	{
-		var curChar = getCurChar();
-		if (!escaping && curChar == endChar) break;
-		if (!escaping && quoted === false && isCharSpecial(curChar)) unexpected();
-
-		if (escaping)
+		if (this.char() == ':') break;
+		if (this.charIsSpace())
 		{
-			switch (getCurChar())
-			{
-				case '"': escaping = false; break;
-				case '\'': escaping = false; break;
-				case '\\': escaping = false; break;
-				case '/': escaping = false; break;
-				case 'b': escaping = false; break;
-				case 'f': escaping = false; break;
-				case 'n': escaping = false; break;
-				case 'r': escaping = false; break;
-				case 't': escaping = false; break;
-				case 'u': res += parseUnicode(); escaping = false; break;
-				default: throw new Error('Unrecognized string escape at ' + lineCount + ':' + curCharI%lineCount + ' (' + getCurChar() + ')!');
-			}
+			this.skipSpaces();
+			this.expect(':');
+			break;
 		}
+		if (this.charIsInvalidForName()) this.expect();
 
-		if (!escaping && curChar == '\\') escaping = true;
-		else res += curChar;
+		res += this.char();
+		this.next();
 
-		if (isCurCharFinal())
-		{
-			if (readingToEnd) break;
-			expectedChar(endChar);
-		}
-		nextChar(true);
+		if (this.charIndex >= this.string.length) this.expect(':');
 	}
 
 	return res;
-};
-
-//Reads a string property's value
-readStringValue = function ()
-{
-	var quoteType = '';
-	if (!(quoteType = isCurCharQuote())) expected('a quote character');
-	nextChar();
-
-	var res = readString(quoteType, true);
-
-	if (!isCurCharQuote(quoteType)) expected(quoteType);
-	nextChar();
-
-	return res;
-};
-
-//Reads a comma separated list to arr ending with endChar
-readListTo = function (ctx, arr, endChar)
-{
-	if (!endChar) endChar = ']';
-	while (true)
-	{
-		if (getCurChar() == endChar) break;
-		arr.push(readValue(ctx));
-
-		if (getCurChar() != ',') break;
-		nextChar();
-	}
-};
-
-//Reads a propertie name/key
-readKey = function ()
-{
-	var enQuoted = false, quoteType = '';
-	if ((quoteType = isCurCharQuote()))
-	{
-		enQuoted = true;
-		nextChar();
-	}
-	
-	if (!enQuoted && isCurCharSpecial()) unexpected();
-
-	var res = '';
-	if (enQuoted)
-	{
-		res = readString(quoteType, true);
-		nextChar();
-
-		expect(':');
-		nextChar();
-	}
-	else
-	{
-		res = readString(':');
-		nextChar();
-	}
-
-	return res;
-};
-
-//Reads an array
-readArray = function (ctx)
-{
-	expect('[');
-	nextChar();
-	var res = [];
-
-	if (getCurChar() == ']')
-	{
-		nextChar();
-		return res;
-	}
-
-	readListTo(ctx, res);
-
-	expect(']');
-	nextChar();
-
-	return res;
-};
-
-//Reads an object
-readObject = function (ctx)
-{
-	expect('{');
-	nextChar();
-	var res = {};
-
-	if (getCurChar() == '}')
-	{
-		if (!isCurCharFinal()) nextChar();
-		return res;
-	}
-
-	while (true)
-	{
-		if (getCurChar() == '}') break;
-		res[readKey()] = readValue(ctx);
-
-		if (getCurChar() != ',') break;
-		nextChar();
-	}
-
-	expect('}');
-	if (!isCurCharFinal()) nextChar();
-
-	return res;
-};
-
-//Parses JSONex handler
-parseHandler = function (ctx)
-{
-	var handlerName = readString('(');
-	expect('(');
-	nextChar();
-
-	if (Object.keys(handlers).indexOf(handlerName) == -1) throw new Error('No such handler (' + handlerName + ') at ' + lineCount + ':' + curCharI%lineCount + '!');	
-	var args = [];
-	args.push(ctx);
-	readListTo(ctx, args, ')');
-
-	expect(')');
-	nextChar();
-
-	return handlers[handlerName].apply(undefined, args);
-};
-
-//Reads a property's value
-readValue = function (ctx)
-{
-	if (isCurCharQuote()) return readStringValue();
-	if (!isNaN(parseInt(getCurChar())) || getCurChar() == '.' || getCurChar() == '-') return readNum();
-	if (getCurChar() == '[') return readArray(ctx);
-	if (getCurChar() == '{') return readObject(ctx);
-	if (!isCurCharSpecial() && !isCurCharSpace()) return parseHandler(ctx);
-	unexpected();
 };
 
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
-var JsonexParser = function (hndlrs, ctx)
+JsonexParser.prototype.illegal = function()
 {
-	if (!hndlrs) hndlrs = {};
-	handlers = hndlrs;
-
-	context = ctx;
+	throw new SyntaxError('Unexpected token ILLEGAL');
 };
 
-//Parses a given string
-JsonexParser.prototype.parse = function (str)
+JsonexParser.prototype.expect = function(char)
 {
-	if (!str) return str;
-	string = str;
-	curCharI = 0;
-	lineCount = 0;
-
-	return readObject(context);
+	if (!this.charIs(char))
+	{
+		if (this.charIndex == this.string.length - 1) throw new SyntaxError('Unexpected end of input');
+		else
+		{
+			if (this.charIs(' ')) throw new SyntaxError('Unexpected token \' \'');
+			else if (this.charIs('\n')) throw new SyntaxError ('Unexpected token \'\\n\'');
+			else if (this.charIs('\t')) throw new SyntaxError ('Unexpected token \'\\t\'');
+			else if (this.charIs('\r')) throw new SyntaxError ('Unexpected token \'\\r\'');
+			else throw new SyntaxError('Unexpected token ' + this.char());
+		}
+	}
 };
 
-module.exports = JsonexParser;
+JsonexParser.prototype.next = function()
+{
+	if (this.charIs('\n')) this.lineNumber++;
+	this.charIndex++;
+};
+
+JsonexParser.prototype.charIsSpecial = function()
+{
+	return this.charIs('[') || this.charIs(']') || this.charIs('{') || this.charIs('}') ||
+		   this.charIs(':') || this.charIs(',') || this.charIs('\'') || this.charIs('"') ||
+		   this.charIs('\\');
+};
+
+JsonexParser.prototype.charIsMathOp = function()
+{
+	return this.charIs('*') || this.charIs('/') || this.charIs('-') || this.charIs('+') ||
+		   this.charIs('^') || this.charIs('!');
+};
+
+JsonexParser.prototype.charIsSpace = function()
+{
+	return this.charIs(' ') || this.charIs('\n') || this.charIs('\r') || this.charIs('\t');
+};
+
+JsonexParser.prototype.charIsInvalidForName = function(isFirst)
+{
+	var res = false;
+	if (isFirst) res = !isNaN(parseInt(this.char()));
+	return this.charIsMathOp() || this.charIsSpecial() || this.charIsSpace() || res;
+};
+
+JsonexParser.prototype.skipSpaces = function()
+{
+	if (this.charIsSpace())
+	{
+		this.next();
+		this.skipSpaces();
+	}
+	if (this.charIs('/'))
+	{
+		this.next();
+		if (this.charIs('/'))
+		{
+			while (!this.charIs('\n') && !this.charIs('\r'))
+			{
+				this.next();
+				if (this.charIndex >= this.string.length) this.expect('\n');
+			}
+			if(!this.charIs('\n') && !this.charIs('\r')) this.expect();
+			this.next();
+			this.skipSpaces();
+		}
+		else if (this.charIs('*'))
+		{
+			while (true)
+			{
+				if (this.charIs('*'))
+				{
+					this.next();
+					if (this.charIs('/')) break;
+				}
+				this.next();
+				if (this.charIndex >= this.string.length) this.expect('*');
+			}
+			this.expect('/');
+			this.next();
+			this.skipSpaces();
+		}
+	}
+};
+
+JsonexParser.prototype.charIs = function(char)
+{
+	return this.string[this.charIndex] == char;
+};
+
+JsonexParser.prototype.char = function()
+{
+	return this.string[this.charIndex];
+};
